4c84e0c1e1622847ae55de5ca525e19582116bb9
qianfeng <qianfeng@chenglaiyun.local>
2016-01-10 10:17:47 +0800

    项目三组

---
+++ FineGoods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FineGoods
+//
+//  Created by qianfeng on 16/1/9.
+//  Copyright © 2016年 Aili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FineGoods/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FineGoods
+//
+//  Created by qianfeng on 16/1/9.
+//  Copyright © 2016年 Aili. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
