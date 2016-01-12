805dc30f091aa6285cd8d99606187e2b12caecc2
jackcheng1993 <qianfeng@chenglaiyun.local>
2016-01-12 10:00:24 +0800

    QINCAI COMMIT

--- FineGoods/Define.h
+++ FineGoods/Define.h
@@ -32,6 +32,6 @@
 
 
 #pragma mark - 请求Url
-#define FHomeUrl @"http://open3.bantangapp.com/recommend/index?app_installtime=1452331892.334167&app_versions=5.3&channel_name=appStore&client_id=bt_app_ios&client_secret=9c1e6634ce1c5098e056628cd66a17a5&device_token=fcd81a592076bcf8880783024f734f690128cf6722f1f54883a256273364722d&oauth_token=a685e481eb1d56727a9c8503ae879781&os_versions=9.2&page=%ld&pagesize=20&screensize=750&track_device_info=iPhone7%2C2&track_deviceid=B884FCB9-2C81-448D-82F4-A28AC8CE7034&track_user_id=1769058&v=10"
+#define FHomeUrl @"http://open3.bantangapp.com/recommend/index?app_installtime=1452331892.334167&app_versions=5.3&channel_name=appStore&client_id=bt_app_ios&client_secret=9c1e6634ce1c5098e056628cd66a17a5&device_token=fcd81a592076bcf8880783024f734f690128cf6722f1f54883a256273364722d&oauth_token=a685e481eb1d56727a9c8503ae879781&os_versions=9.2&page=%ld&pagesize=20&screensize=750&track_deviceid=B884FCB9-2C81-448D-82F4-A28AC8CE7034&track_user_id=1769058&v=10"
 
 #endif /* Define_h */
