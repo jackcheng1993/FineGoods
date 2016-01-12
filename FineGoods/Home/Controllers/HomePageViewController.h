7fd59888a5dae7836df5a3f4cb48dc0ea087e44b
qianfeng <qianfeng@Lucky.local>
2016-01-12 09:42:29 +0800

    Aili eight

---
+++ FineGoods/Home/Controllers/HomePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomePageViewController.h
+//  FineGoods
+//
+//  Created by qianfeng on 16/1/12.
+//  Copyright © 2016年 Aili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePageViewController : UITableViewController
+
+@end
